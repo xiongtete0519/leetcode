ac9b0c7725fe5924afd00d5591fea864ecd6511d
CodeLeader <1144722582@qq.com>
2023-02-19 15:07:14 +0800

    169多数元素(HashMap/排序)

--- src/leetcode1/LeetCode169.java
+++ src/leetcode1/LeetCode169.java
@@ -1,14 +1,12 @@
 package leetcode1;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 多数元素
  */
 public class LeetCode169 {
+    //方法一：哈希表
     public static int majorityElement(int[] nums) {
         //使用HashMap存储每个元素以及出现的次数
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -30,8 +28,19 @@ public class LeetCode169 {
         return majorityEntry.getKey();
 
     }
+    /**
+     * 方法二：排序
+     * 如果将数组 nums 中的所有元素按照单调递增或单调递减的顺序排序，
+     * 那么下标为n/2(下标从0开始)一定是众数
+     */
+    public static Integer majorityElement1(int[] nums){
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+
 
     public static void main(String[] args) {
         System.out.println(majorityElement(new int[]{3, 2, 3}));
+        System.out.println(majorityElement1(new int[]{3, 2, 3}));
     }
 }
