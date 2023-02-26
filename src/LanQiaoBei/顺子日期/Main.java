cfa32bf0f9ab2050af0cbba8221dbd6d0ca94c03
CodeLeader <1144722582@qq.com>
2023-02-26 12:15:53 +0800

    蓝桥杯-顺子日期

---
+++ src/LanQiaoBei/顺子日期/Main.java
@@ -0,0 +1,26 @@
+package LanQiaoBei.顺子日期;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] seq={
+                "012", "123","234","345","456","567","678","789"
+        };
+        LocalDate localDate = LocalDate.of(2022, 1, 1);
+        Integer count=0;
+        while(localDate.getYear()==2022){
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+            String format = dtf.format(localDate);
+            for (String s : seq) {
+                if(format.contains(s)){
+                    count++;
+                    break;//每个日期只被统计一次,20220123这个会被统计两次
+                }
+            }
+            localDate= localDate.plusDays(1);
+        }
+        System.out.println(count);
+    }
+}
