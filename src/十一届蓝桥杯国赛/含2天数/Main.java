f0217f0e5b9a160b45cf7271cbd43622a7b08053
CodeLeader <1144722582@qq.com>
2023-05-04 22:16:23 +0800

    十一届国赛：合数个数、含2天数(LocalDate)、阶乘约数(约数定理)

---
+++ src/十一届蓝桥杯国赛/含2天数/Main.java
@@ -0,0 +1,25 @@
+package 十一届蓝桥杯国赛.含2天数;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate start = LocalDate.of(1900, 1, 1);
+        LocalDate end = LocalDate.of(9999, 12, 31);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+        long res=0;
+        while(true){
+            String s = dtf.format(start);
+            //判断是否含有2
+            if(s.contains("2")){
+                res++;
+            }
+            if(start.getYear()==end.getYear()&&start.getMonth()==end.getMonth()&&start.getDayOfMonth()==end.getDayOfMonth()){
+                break;
+            }
+            start=start.plusDays(1);
+        }
+        System.out.println(res);
+    }
+}
