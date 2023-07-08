5808f8f93a8eb0784c05e13928e062a7511defc5
CodeLeader <1144722582@qq.com>
2023-07-08 09:28:52 +0800

    代码随想录：字符串-左旋转字符串

---
+++ src/代码随想录/左旋转字符串/Main.java
@@ -0,0 +1,15 @@
+package 代码随想录.左旋转字符串;
+
+public class Main {
+    public static String reverseLeftWords(String s, int n) {
+        String s1 = s.substring(0, n);
+        String s2 = s.substring(n);
+        return s2+s1;
+    }
+
+    public static void main(String[] args) {
+        String s="abcdefg";
+        int k=2;
+        System.out.println(reverseLeftWords(s, k));
+    }
+}
